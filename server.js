5ccc9787c2cbc045b2e78945f707ba2276b9ea02
Zack <zackgall@gmail.com>
2017-05-15 14:01:36 +0000

    thought it would be a good point to save work before tinkering

--- server.js
+++ server.js
@@ -1,77 +1,68 @@
-//Dependencies
 var twitter = require('ntwitter');
 var util = require('util');
 var credentials = require('./credentials.js');
-var cloudinary = require('cloudinary');
-var MongoClient = require('mongodb').MongoClient
-    , format = require('util').format;
-var moment = require('moment'); 
-    
+var MongoClient = require('mongodb').MongoClient,
+    format = require('util').format;
+var moment = require('moment');
+var mongoURL = 'mongodb://fxadmin:Password1@ds133231.mlab.com:33231/replaytweets'
+var orderCollected = 0;
+
+//define find method
+
+
 // Twitter Stream Topics
-var trackedTags = ['#selfie']; // topics
+var trackedTags = ['#AcademyPgh', '#AcademyPgh3']; // topics
 
 // oauth twitter client
 var twit = new twitter({
-  		consumer_key: credentials.consumer_key,
-  		consumer_secret: credentials.consumer_secret,
-  		access_token_key: credentials.access_token_key,
-  		access_token_secret: credentials.access_token_secret
+    consumer_key: credentials.consumer_key,
+    consumer_secret: credentials.consumer_secret,
+    access_token_key: credentials.access_token_key,
+    access_token_secret: credentials.access_token_secret
 });
 
-//cloudniary API keys
-cloudinary.config({ 
-  cloud_name: 'replayfx', 
-  api_key: '286445638368727', 
-  api_secret: '9exIU9hCrBnUas3akRjmcMKO7jA' 
-});
-
-                   
 
 
 
-// MongoDB Client Connection
+// MongoDB Client Connection - To your Local or remote MongoDB Database
+// NOTE: The name is at the end of the connection : streamland
 MongoClient.connect('mongodb://fxadmin:Password1@ds133231.mlab.com:33231/replaytweets', function(err, db) {
-    if(err) {console.log(err);}     
-    
+    if (err) {
+        console.log(err);
+    }
+
     // create mongodb collection
     var collection = db.collection('fxtweets');
 
-	  	
     // outside loop into stream
-    twit.stream('statuses/filter',  { track: trackedTags}, function(stream) {
-            // console.log(trackedTags);
-            stream.on('data', function (data) {
-                // look for your topic, write that tweet to mongodb
-                trackedTags.forEach(function (trackedTags) {
-                    //run RegEx on parameters to replace illegal characters.
-                        var myRegExp = new RegExp(trackedTags)
-                        //if text matches our input topics AND contains media
-                        if (data.text.match(myRegExp) && data.entities.media != null) {
-                            console.log(data);
-                            console.log(data.entities.media[0].media_url)
-                            
+    twit.stream('statuses/filter', {
+        track: trackedTags
+    }, function(stream) {
+        // console.log(trackedTags);
+        stream.on('data', function(data) {
+            // look for your topic, write that tweet to mongodb
+            trackedTags.forEach(function(trackedTags) {
+                var myRegExp = new RegExp(trackedTags)
+                    //if text matches our input topics AND contains media
+                if (data.text.match(myRegExp) && data.entities.media != null) {
 
-//upload images to cloudinary         
-//replace tweetImage with link to cloudinary
-
-                        // insert mongodb record
-
-                        collection.insert({'postDT': moment().format('MM-DD-YYYY HH:MM'), // GEO?
-                                           'tweetID': data.id_str,
-                                           'userID': data.user.id_str,
-                                           'userName': data.user.screen_name, 
-                                           'userImage': data.user.profile_image_url,
-                                           'tweetImage': data.entities.media[0].media_url,
-                                           'tweetText': data.text}, function(err, result) {
-                                   if (err) { console.log(err);}
+                    // insert mongodb record
+                    // insert tweet -- mongo collection
+                    collection.insert({
+                            'postDT': moment().format('MM-DD-YYYY HH:MM'), // GEO?
+                            'userName': data.user.screen_name,
+                            'userImage': data.user.profile_image_url,
+                            'tweetImage': data.entities.media[0].media_url_https,
+                            'tweetText': data.text
+                        },
+                        function(err, result) {
+                            if (err) {
+                                console.log(err);
+                            }
                         });
-                        //
-                        console.log("TWEET CAPTURED! " + moment().format('MM-DD-YYYY HH:MM') +" From: "+ data.user.screen_name);    
-                        //
-                        } 
-                 });
+                    console.log("TWEET CAPTURED! " + moment().format('MM-DD-YYYY HH:MM') + " From: " + data.user.screen_name);
+                }
             });
-    });
-});
-//
-//
+        });
+    }); //close function(stream)
+}); // close mongodb
